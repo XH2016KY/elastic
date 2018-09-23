55165edc0f49e88b6df82d9335e7952bb4ef2b74
XH2016KY <javaemployee2015@163.com>
2018-09-23 21:26:00 +0800

    "Restful服务接口"

--- elastic-service/src/main/java/com/elastic/controller/ProductController.java
+++ elastic-service/src/main/java/com/elastic/controller/ProductController.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
@@ -17,6 +19,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -103,6 +106,54 @@ public class ProductController {
 		return new ResponseEntity(result,HttpStatus.OK);
 		
 	}
-	
+	/**
+	 * 根据id修改索引
+	 * @param id
+	 * @param name
+	 * @param age
+	 * @param keyword
+	 * @param date
+	 * @return
+	 */
+	@PutMapping(value = "update/pepole/man")
+	@ResponseBody
+	public ResponseEntity update(
+			   @RequestParam(name = "id")String id,
+			   @RequestParam(name = "name",required = false)String name,
+			   @RequestParam(name = "age",required = false)Integer age,
+			   @RequestParam(name = "country",required = false)String keyword,
+			   @RequestParam(name ="date",required = false)@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+			   Date date
+			){
+		       UpdateRequest update = new UpdateRequest("people","man",id);
+		       try {
+				XContentBuilder builder = XContentFactory.jsonBuilder()
+				   .startObject();
+				if(name != null) {
+					builder.field("name",name);
+				}
+				if(age != null) {
+					builder.field("age",age);
+				}
+				if(keyword != null) {
+					builder.field("country",keyword);
+				}
+				if(date != null) {
+					builder.field("date",date.getTime());
+				}
+				builder.endObject();
+				update.doc(builder);
+			} catch (IOException e) {
+				e.printStackTrace();
+				return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+		       try {
+				UpdateResponse result = this.client.update(update).get();
+				return new ResponseEntity(result,HttpStatus.OK);
+			} catch (Exception e) {
+				e.printStackTrace();
+				return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+	}
 
 }
